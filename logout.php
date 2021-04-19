b90ccee091a17a64402d5aaaa17b7b92bbaa1ef6
Charlie Seoane <seosoft@1informatica16.mindef.local>
2021-04-19 10:03:27 -0400

    Actualiace el logout

--- logout.php
+++ logout.php
@@ -1,5 +1,6 @@
 <?
 session_start();
+
 unset($_SESSION["authsialma"],$_SESSION["sialmamodulo"],$_SESSION["sialmanombre"],$_SESSION["sialmausr"],$_SESSION["sialmarol"],$_SESSION["sialmagalm"]);
 ?>
 <script language="JavaScript" type="text/javascript">
