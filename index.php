d93a4fc8719346ab5364f90f5a3c5e587cb6fd38
m4r1o20 <60524932+m4r1o20@users.noreply.github.com>
2021-04-19 10:15:12 -0400

    hola soy tu amo

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 <meta name="author" content="">
-<title>Sistema de Almacenes</title>
+<title>Sistema de Almacenes PUTOS</title>
 
 <!-- Bootstrap Core CSS -->
 <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -26,18 +26,18 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-<!-- jQuery --> 
-<script src="vendor/jquery/jquery.min.js"></script> 
+<!-- jQuery -->
+<script src="vendor/jquery/jquery.min.js"></script>
 
-<!-- Bootstrap Core JavaScript --> 
-<script src="vendor/bootstrap/js/bootstrap.min.js"></script> 
+<!-- Bootstrap Core JavaScript -->
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 
-<!-- Metis Menu Plugin JavaScript --> 
-<script src="vendor/metisMenu/metisMenu.min.js"></script> 
+<!-- Metis Menu Plugin JavaScript -->
+<script src="vendor/metisMenu/metisMenu.min.js"></script>
 
-<!-- Custom Theme JavaScript --> 
+<!-- Custom Theme JavaScript -->
 <script src="resources/scripts/sb-admin-2/js/sb-admin-2.js"></script>
-  
+
 </head>
 
 <body>
